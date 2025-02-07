ff68fc149d25db40ae71ce1e8f4925bd321fdcf4
davidrburt <dburt@mit.edu>
2025-02-06 19:11:41 -0500

    adding plotting of covariates

--- experiments/real_data/run.sh
+++ experiments/real_data/run.sh
@@ -1,4 +1,6 @@
 python save_data.py
+python plot_tree_cover_covariates.py 
+
 python tree_cover_exp.py --"num_neighbors"=1 --"lipschitz_bound"=0.2  --"num_threads"=36 --"num_seeds"=250 --"seeds_to_plot"=7 --"region"="south"
 python tree_cover_exp.py --"num_neighbors"=1 --"lipschitz_bound"=0.2  --"num_threads"=36 --"num_seeds"=250 --"seeds_to_plot"=7 --"region"="west"
 
